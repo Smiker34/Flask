43cd75107759572e283e8162dc98e71db4ad29ae
Амир <Smiker1@yandex.ru>
2023-04-28 10:11:59 +0300

    tasks

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,24 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return "It's work!"
+
+from article.views import article
+from user.views import user
+from index.views import index
+from report.views import report
+
+VIEWS = [
+    index,
+    user,
+    article,
+    report
+]
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app: Flask):
+    for view in VIEWS:
+        app.register_blueprint(view)
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from blog.app import app
+from gb_web.app import create_app
+
+
 if __name__ == "__main__":
+    app = create_app()
     app.run(
-    host="0.0.0.0",
-    debug=True,
-)
+        host="0.0.0.0",
+        port=8000,
+        debug=True
+    )
