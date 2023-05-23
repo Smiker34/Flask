a3b751a16c435c632b1db9c2e9657b1e298671f4
Амир <Smiker1@yandex.ru>
2023-05-23 10:13:06 +0300

    tasks

--- blog/models/article.py
+++ blog/models/article.py
@@ -1,19 +1,21 @@
-from datetime import datetime
-
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-
-from ..app import db
-
+from blog.models.database import db
+from datetime import datetime
+from sqlalchemy import Column, Integer, ForeignKey, String, Text, DateTime, func
+from blog.models.article_tag import article_tag_association_table
 
 class Article(db.Model):
-    __tablename__ = "articles"
-
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, ForeignKey("authors.id"), nullable=False)
-    title = db.Column(db.String(255))
-    text = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey("author.id"))
     author = relationship("Author", back_populates="articles")
+    title = Column(String(200), nullable=False, default="", server_default="")
+    body = Column(Text, nullable=False, default="", server_default="")
+    dt_created = Column(DateTime, default=datetime.utcnow, server_default=func.now())
+    dt_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    tags = relationship(
+        "Tag",
+        secondary=article_tag_association_table,
+        back_populates="articles",
+    )
+
--- blog/models/user.py
+++ blog/models/user.py
@@ -1,22 +1,32 @@
+from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
+from blog.security import flask_bcrypt
+from blog.models.database import db
 from flask_login import UserMixin
 from sqlalchemy.orm import relationship
 
-from ..app import db
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    is_staff = Column(Boolean, nullable=False, default=False)
+    first_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    last_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    email = Column(String(255), unique=True, nullable=False, default="", server_default="")
+    _password = Column(LargeBinary, nullable=True)
     author = relationship("Author", uselist=False, back_populates="user")
 
-    def __init__(self, email, password, first_name, last_name):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = flask_bcrypt.generate_password_hash(value)
+
+    def validate_password(self, password) -> bool:
+        return flask_bcrypt.check_password_hash(self._password, password)
+
+
+    def __repr__(self):
+        return f"<User #{self.id} {self.username!r}>"
